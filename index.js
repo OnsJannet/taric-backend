9fc3fee8e85be284d0ade25aa1f56138f851a283
OnsJannet <onsjannet@gmail.com>
2025-01-09 20:41:54 +0100

    taric code backend

--- index.js
+++ index.js
@@ -23,8 +23,15 @@ const port = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
+
+
 // Middleware
-app.use(cors());
+app.use(cors({ 
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow all methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow all headers
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
